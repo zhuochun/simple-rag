96bcf91540fe424f9b09fad1bf5ecf20cf680748
Zhuochun <zhuochun@hotmail.com>
2024-08-09 23:02:04 +0800

    Add an experimental feature

--- llm/openai.rb
+++ llm/openai.rb
@@ -7,7 +7,7 @@ NEXT_ROLE = ->(role) { role != ROLE_USER ? ROLE_USER : ROLE_ASSISTANT }
 
 def chat(messages, opts = {})
   data = {
-    "model" => "gpt-3.5-turbo-16k",
+    "model" => "gpt-4o-mini",
     "messages" => messages
   }.merge(opts)
 
